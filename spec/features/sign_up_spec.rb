5376626f1a6a10bde687e42af2d6cc26d4eddb34
Liz Daly <liz7daly@gmail.com>
2020-04-01 16:30:32 +0100

    Add feature tests for sign in
    Tests ensure that a user cannot create a route without logging in, cannot log in
    without signing up.

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Sign up', type: :feature do
   scenario 'new user signs up' do
     visit '/'
-    click_on 'Sign up'
     sign_up('name', 'username', 'test@example.com', 'password')
     expect(page).to have_content 'test@example.com'
     expect(current_path).to eq '/'
   end
 
-  scenario 'new user signs up' do
+  scenario 'user must enter valid email' do
     visit '/'
-    click_on 'Sign up'
     sign_up('name', 'username', 'testexample.com', 'password')
     expect(page).to have_content 'Email is invalid'
   end
