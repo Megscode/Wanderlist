509663fcc44852bedb48c5712a63c213b5f37727
Liz Daly <liz7daly@gmail.com>
2020-04-01 12:36:56 +0100

    Add name and username to user
    Adds new columns to user table and enables new user fields through devise.
    
    Signing up now takes users to a landing page.
    
    Tests currently failing due to default tests on welcome index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
   resources :routes
   resources :places
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
