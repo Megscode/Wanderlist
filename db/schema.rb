38ca1461fd78bc07df9fb71ad46805e0ade0a9b4
Megan Hayes <student@makerss-air.home>
2020-04-01 12:24:07 +0100

    revert back to commit 1

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_03_31_140306) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "sqlite"
 
   create_table "places", force: :cascade do |t|
     t.string "name"
