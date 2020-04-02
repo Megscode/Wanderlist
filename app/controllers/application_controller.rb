7b706d45959835ed5daba68e9f61d5b8f703dd08
Liz Daly <liz7daly@gmail.com>
2020-04-02 15:18:33 +0100

    Remove commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
-    # protect_from_forgery with: :exception
-    # before_action :authenticate_user!
-    # before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-    protected
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :username, :email, :password) }
-        end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :username, :email, :password) }
+  end
 end
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,6 +1,5 @@
 class RoutesController < ApplicationController
   before_action :set_route, only: [:show, :edit, :update, :destroy]
-  # skip_before_action :authenticate_user!, only: [:index]
   protect_from_forgery with: :exception
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :configure_permitted_parameters, if: :devise_controller?
