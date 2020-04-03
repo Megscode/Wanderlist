5a94b5543311622567758cd3468bf51431ff9c1b
Liz Daly <liz7daly@gmail.com>
2020-04-03 12:37:35 +0100

    Add JS for search box

---
+++ app/javascript/custom/createRoute.js
@@ -0,0 +1,100 @@
+window.initMap= function() {
+  this.console.log("loading")
+  var mapOptions = {zoom: 14, center: {lat: 51.5428627, lng: -0.039212}, mapTypeId: 'roadmap'}
+
+  var map = new google.maps.Map(
+    document.getElementById('map'), mapOptions);
+
+  // refreshPoints()
+
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  var addPlace = document.getElementById('add-marker')
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  
+  var markers = []
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+  
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      addPlace.addEventListener('click', function() {
+        console.log('this is clicking')
+      })
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+
+    });
+    map.fitBounds(bounds);
+
+    
+  });
+
+
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.latLng, 
+      map: map,
+      draggable:true,
+      // title:"Drag me!"
+    })
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: `<h1>${props.header}</h1><p>${props.description}</p>`
+    })
+
+    
+
+    marker.addListener('click', function() {
+      infoWindow.open(map, marker)
+    })
+
+  }
+  // function refreshPoints() {
+  //   for(var i = 0; i < itinerary.points.length; i++) {
+  //     addMarker(itinerary.points[i])
+  //   }
+  }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("custom/test")
-
-import Hello from 'custom/test' 
-window.Hello = Hello 
+require("custom/createRoute");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
