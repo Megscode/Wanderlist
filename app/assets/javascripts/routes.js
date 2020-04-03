40874c38913ce0b1e3686384f92f5c66471ac58b
Richard Pattinson <richie.pattinson@googlemail.com>
2020-04-03 14:59:12 +0100

    3 markers on the map

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -5,13 +5,18 @@ function initMap(lat, lng, array) {
         zoom: 14
     };
 
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+    // var marker = new google.maps.Marker({
+    //     position: myCoords,
+    //     map: map})
+}
+function initMarker(lat, lng){
+    var myCoords = new google.maps.LatLng(lat, lng);
     var marker = new google.maps.Marker({
         position: myCoords,
         map: map
-    });
-
+    })
 }
 
 
