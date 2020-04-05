0d556500c2fcefd099fb2789425c026fe9908ee0
Richard Pattinson <richie.pattinson@googlemail.com>
2020-04-05 15:21:05 +0100

    directions work with dummy dataset

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -6,34 +6,35 @@ function initMap(lat, lng) {
     };
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-function initDirections(){
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
     directionsDisplay.setMap(map)
     origin = new google.maps.LatLng(51.501564, -0.141944)
     destination = new google.maps.LatLng(51.500792, -0.124613)
-    waypoint = new google.maps.LatLng(51.499581, -0.127309) 
+    waypoint = [{location: new google.maps.LatLng(51.499581, -0.127309), stopover: true}]
+    document.getElementById("get_directions").addEventListener("click", function(){
+        console.log("something")
+        calculateRoute()
+    })
 }
+
 function calculateRoute(){
     var request = {
         origin: origin,
         destination: destination,
-        travelMode: 'WALKING',
-        waypoints: [waypoint]
+        waypoints: waypoint,
+        travelMode: 'WALKING'
     };
     directionsService.route(request, function(result, status){
-        console.log(result, status)
+        if(status === "OK"){
+            directionsDisplay.setDirections(result)
+            }
+        else{
+            window.alert('Directions request failed due to ' + status);
+        }
     })
 }
-}
 
-document.getElementById("get_directions").addEventListener("click", function(){
-    console.log("something")
-    initDirections()
-    calculateRoute()
-})
 function initMarker(lat, lng){
     var myCoords = new google.maps.LatLng(lat, lng);
     var marker = new google.maps.Marker({
