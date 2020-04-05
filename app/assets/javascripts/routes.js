6178332f532b63bc487c3a2f5bb63c8b2569a46c
Richard Pattinson <richie.pattinson@googlemail.com>
2020-04-05 11:36:31 +0100

    add init directions structure to routes.js - not working yet

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -6,8 +6,34 @@ function initMap(lat, lng) {
     };
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+
+function initDirections(){
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map)
+    origin = new google.maps.LatLng(51.501564, -0.141944)
+    destination = new google.maps.LatLng(51.500792, -0.124613)
+    waypoint = new google.maps.LatLng(51.499581, -0.127309) 
+}
+function calculateRoute(){
+    var request = {
+        origin: origin,
+        destination: destination,
+        travelMode: 'WALKING',
+        waypoints: [waypoint]
+    };
+    directionsService.route(request, function(result, status){
+        console.log(result, status)
+    })
+}
 }
 
+document.getElementById("get_directions").addEventListener("click", function(){
+    console.log("something")
+    initDirections()
+    calculateRoute()
+})
 function initMarker(lat, lng){
     var myCoords = new google.maps.LatLng(lat, lng);
     var marker = new google.maps.Marker({
@@ -15,10 +41,3 @@ function initMarker(lat, lng){
         map: map
     })
 }
-function initDirections(placesArray){
-    var directionsService = new google.maps.DirectionsService;
-    var directionRequest = new google.maps.DirectionsRequest()
-    directionsService.route()
-    
-}
-
