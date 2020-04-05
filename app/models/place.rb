0b3798b7d1ecd2efd0625d2e2ba3ec81270a4605
Liz Daly <liz7daly@gmail.com>
2020-04-05 15:59:14 +0100

    Extract create many places to place model
    Create method on routes controller is currently doing a lot of work and so
    extracting several methods to model. New method 'create_or_find_many' on place
    model will loop through a hash of places received from an ajax request and
    create an array of place objects.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,9 @@
 class Place < ApplicationRecord
   has_many :routes
+
+  def self.create_or_find_many(places)
+    places.each do |k, place|
+      places.push(Place.create(name: place['name'], description: place['description'], latitude: place['latitude'].to_f, longitude: place['longitude'].to_f, google_places_id: place['google_places_id']))
+    end
+  end
 end
