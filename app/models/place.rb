c398593ba40b2fe89d55efee10dbde71c0d0984b
Liz Daly <liz7daly@gmail.com>
2020-04-05 16:35:10 +0100

    Create array of places

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,10 @@ class Place < ApplicationRecord
   has_many :routes
 
   def self.create_or_find_many(places)
+    array_of_places = []
     places.each do |k, place|
-      places.push(Place.create(name: place['name'], description: place['description'], latitude: place['latitude'].to_f, longitude: place['longitude'].to_f, google_places_id: place['google_places_id']))
+      array_of_places.push(Place.create(name: place['name'], description: place['description'], latitude: place['latitude'].to_f, longitude: place['longitude'].to_f, google_places_id: place['google_places_id']))
     end
+    array_of_places
   end
 end
