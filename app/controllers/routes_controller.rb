6cb8a4f2d9a835c3f04ba5ac3a479c26741eca00
Liz Daly <liz7daly@gmail.com>
2020-04-06 22:14:30 +0100

    Delete commented code

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,7 +1,7 @@
 class RoutesController < ApplicationController
   before_action :set_route, only: [:show, :edit, :update, :destroy]
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy];
 
   # GET /routes
   # GET /routes.json
