7f0a06d19985537a0bfc8042cd2b0990e9b78a7c
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-06 12:47:58 +0100

    Full functionality added

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :route
-  # validates :user_id, uniqueness: { scope: :route_id }
+  validates :user_id, uniqueness: { scope: :route_id }
 end
