82b3dc5557531150a61f90e0c0d11f5713f1f695
Dafna Libman <dafnalibman@gmail.com>
2020-04-07 16:25:29 +0100

    Add autocomplete to search location box

--- app/javascript/maps/current_location.js
+++ app/javascript/maps/current_location.js
@@ -10,7 +10,42 @@ window.initUserLocation = function() {
         document.location.href='http://localhost:3000/nearby?lat='+pos.lat+'&lng='+ pos.lng
       })
     }
-
   })
+    var input = document.getElementById('pac-input');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    var grab_place = document.getElementById('submit')
+    grab_place.addEventListener('click', initAutoComplete)
+
+    function initAutoComplete() {
+      var input = document.getElementById('pac-input');
+      var autocomplete = new google.maps.places.SearchBox(input);
+      console.log(autocomplete.getPlace())
+      lat = autocomplete.getPlace().location.lat
+      console.log(lat)
+      lng = autocomplete.getPlace().location.lng
+      // document.addEventListener('lat')
+    }
+    // google.maps.event.addDomListener(window, 'load', initAutocomplete);
+  // }
 }
 export default initUserLocation
+
+// function addPlace() {
+//   if (route.length === 8) {
+//     alert("You can only add up to 8 places to a route")
+//   } else {
+//     var newPlace = searchBox.getPlaces()[0]
+//     var placeParams = { 
+//       name: newPlace.name, 
+//       description: newPlace.formatted_address, 
+//       latitude: newPlace.geometry.location.lat, 
+//       longitude: newPlace.geometry.location.lng,
+//       google_places_id: newPlace.place_id
+//     }
+
+//     addMarker(newPlace)
+//     input.value = ""
+//     route.push(placeParams)
+//     printRoute()
+//   } 
--- app/javascript/maps/nearby.js
+++ app/javascript/maps/nearby.js
@@ -1,24 +1,19 @@
 window.initMapNearBy= function(lat, lng, route_array){
   var routes = route_array
   var myCoords = new google.maps.LatLng(lat, lng);
-
   var mapOptions = {
       center: myCoords,
       zoom: 14
   };
-
   console.log(mapOptions);
   var map = new google.maps.Map(document.getElementById('map-nearby'), mapOptions);
   initCurrentLocationMarker(lat, lng)
   addAllMarkers(routes)
-
-
   function addAllMarkers(route) {
     for(var i = 0; i < route.length; i++) {
         initMarker(route[i])
     }
   }
-
   function initMarker(lat, lng, title){
       var myCoords = new google.maps.LatLng(lat, lng);
       var marker = new google.maps.Marker({
@@ -28,12 +23,10 @@ window.initMapNearBy= function(lat, lng, route_array){
       var infoWindow = new google.maps.InfoWindow({
         content: `<h1>${title}</h1>`
       })
-  
       marker.addListener('click', function() {
         infoWindow.open(map, marker)
       })
   }
-
   function initCurrentLocationMarker(lat, lng){
     var myCoords = new google.maps.LatLng(lat, lng);
     var marker = new google.maps.Marker({
@@ -42,11 +35,5 @@ window.initMapNearBy= function(lat, lng, route_array){
         map: map
     })
   }
-  
-
 }
-
 export default initMapNearBy
-
-
-
