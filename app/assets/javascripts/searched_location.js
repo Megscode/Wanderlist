fcbd3362affe347eb70954b8c06367dd340a4ca2
Dafna Libman <dafnalibman@gmail.com>
2020-04-07 14:26:28 +0100

    Add search place - work in progress

--- app/assets/javascripts/searched_location.js
+++ app/assets/javascripts/searched_location.js
@@ -1,12 +1,20 @@
 window.initSearchedLocation = function() {
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  document.getElementById('submit').addEventListener('click', function(){
-    console.log("we just clicked our submit and this was triggered")
-  });
-  };
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
+  var route = [];
 
-export default initSearchedLocation
+  document.getElementById('submit').addEventListener('click', function() {
+    console.log("we just clicked the submit button!")
+  // searchBox.addListener('places_changed', grabPlace);
 
+  // // function addPlace() {
+  // //     var newPlace = searchBox.getPlaces()[0]
+  // //     var placeParams = { 
+  // //       name: newPlace.name, 
+  // //       description: newPlace.formatted_address, 
+  // //       latitude: newPlace.geometry.location.lat, 
+  // //       longitude: newPlace.geometry.location.lng,
+  // //       google_places_id: newPlace.place_id
+  // //     }
+ }
+ export default initSearchedLocation
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("maps/search")
+require("searched_location")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
