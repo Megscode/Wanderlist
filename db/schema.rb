a64c481fde7a5b974007231b77350d9c10c5df23
Megan Hayes <student@makerss-air.home>
2020-04-07 21:55:59 +0100

    navbar links set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_104945) do
+ActiveRecord::Schema.define(version: 2020_04_06_152025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_104945) do
     t.integer "place8_ID"
     t.bigint "user_id"
     t.string "photo_url"
+    t.string "picture_url"
     t.integer "points", default: 1
     t.float "hot_score", default: 0.0
     t.index ["user_id"], name: "index_routes_on_user_id"
