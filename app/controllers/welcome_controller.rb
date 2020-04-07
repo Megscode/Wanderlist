637f440af62fce8df029dd878082a2f7efb337a2
Richard Pattinson <richie.pattinson@googlemail.com>
2020-04-07 13:58:18 +0100

    routes nearby page now displays nearby routes:

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
   end
 
   def nearby
-    @lat = params[:lat].to_i
-    @lng = params[:lng].to_i
+    @lat = params[:lat].to_f
+    @lng = params[:lng].to_f
     @tolerance = 0.031851 
     @routes = Route.where(
       "starting_lat > ? AND starting_lat < ?", @lat-@tolerance, @lat+@tolerance).where("starting_lng > ? AND starting_lng < ?", @lng-@tolerance, @lng+@tolerance)
--- app/javascript/maps/nearby.js
+++ app/javascript/maps/nearby.js
@@ -1,17 +1,45 @@
-window.initMapNearBy= function(lat, lng){
+window.initMapNearBy= function(lat, lng, route_array){
+  var routes = route_array
   var myCoords = new google.maps.LatLng(lat, lng);
-  console.log(myCoords);
+
   var mapOptions = {
       center: myCoords,
       zoom: 14
   };
+
   console.log(mapOptions);
   var map = new google.maps.Map(document.getElementById('map-nearby'), mapOptions);
-  var marker = new google.maps.Marker({
-      position: myCoords,
-      map: map
-  })
+  initCurrentLocationMarker(lat, lng)
+  addAllMarkers(routes)
+
+
+  function addAllMarkers(route) {
+    for(var i = 0; i < route.length; i++) {
+        initMarker(route[i])
+    }
+  }
+
+  function initMarker(lat, lng, title){
+      var myCoords = new google.maps.LatLng(lat, lng);
+      var marker = new google.maps.Marker({
+          position: myCoords,
+          label: `${title}`,
+          map: map
+      })
+  }
+
+  function initCurrentLocationMarker(lat, lng){
+    var myCoords = new google.maps.LatLng(lat, lng);
+    var marker = new google.maps.Marker({
+        position: myCoords,
+        label: "Your Location",
+        map: map
+    })
+  }
+  
+
 }
+
 export default initMapNearBy
 
 
