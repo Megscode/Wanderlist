01412bbab9303040e73c3743137551e75089a388
Liz Daly <liz7daly@gmail.com>
2020-04-09 08:06:54 +0100

    Remove places controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   resources :routes do
     post :upvote, on: :member
   end
-  resources :places
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
